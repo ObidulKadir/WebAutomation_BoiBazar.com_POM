567142ec77fe85f8fdfd5ca7bfc6aad7963df2b5
S.K.Obidul kadir <rezwankadirobaidul@gmail.com>
2023-11-12 13:40:41 +0600

    BaseDriver and PageDriver setup

---
+++ src/test/java/BaseDrivers/baseDriver.java
@@ -0,0 +1,42 @@
+package BaseDrivers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class baseDriver {
+	protected static String url = "https://www.boibazar.com/";
+	WebDriver driver;
+	
+	@BeforeSuite
+	public void StartBrowser() {
+		String browserName = System.getProperty("browser", "chrome");
+		
+		if(browserName.equals("chrome")){
+			
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+	
+	}else if(browserName.equals("edge")) {
+		WebDriverManager.edgedriver().setup();
+		driver = new EdgeDriver();
+	}
+	else {
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+	}
+		PageDriver.getInstance().setDriver(driver);
+	
+	}
+	
+	@AfterSuite
+	public void CloseBrowser() {
+		driver.quit();
+	}
+
+}
